8c273209e09bd28ad662ecd5588e94e2e51242c2
Josh Williams <josh@spacesnottabs.com>
2023-01-29 21:30:40 +0000

    Added threading for concurrent file processing

--- lib/register_transformer_psc/apps/transformer_bulk.rb
+++ lib/register_transformer_psc/apps/transformer_bulk.rb
@@ -15,6 +15,7 @@ module RegisterTransformerPsc
     class TransformerBulk
       BATCH_SIZE = 25
       NAMESPACE = 'PSC_TRANSFORMER_BULK'
+      PARALLEL_FILES = ENV.fetch("PSC_PARALLEL_FILES", 3).to_i
 
       def self.bash_call(args)
         s3_prefix = args.last
@@ -40,24 +41,32 @@ module RegisterTransformerPsc
         s3_paths = s3_adapter.list_objects(s3_bucket: s3_bucket, s3_prefix: s3_prefix)
         print("GOT #{s3_paths.length} S3 Paths: ", s3_paths, "\n")
 
-        s3_paths.each do |s3_path|
-          print "Trying #{s3_path}\n"
-          if file_processed?(s3_path)
-            print "Skipping #{s3_path}\n"
+        s3_paths.each_slice(PARALLEL_FILES) do |s3_paths_batch|
+          threads = []
+          s3_paths_batch.each do |s3_path|
+            threads << Thread.new { process_s3_path(s3_path) }
           end
-
-          file_reader.read_from_s3(s3_bucket: s3_bucket, s3_path: s3_path) do |rows|
-            process_rows rows
-          end
-
-          mark_file_complete(s3_path)
-          print "Completed #{s3_path}\n"
+          threads.each(&:join)
         end
       end
 
       private
 
       attr_reader :bods_mapper, :redis, :s3_bucket, :s3_adapter, :file_reader
+
+      def process_s3_path(s3_path)
+        print "Trying #{s3_path}\n"
+        if file_processed?(s3_path)
+          print "Skipping #{s3_path}\n"
+        end
+
+        file_reader.read_from_s3(s3_bucket: s3_bucket, s3_path: s3_path) do |rows|
+          process_rows rows
+        end
+
+        mark_file_complete(s3_path)
+        print "Completed #{s3_path}\n"
+      end
 
       def process_rows(rows)
         records = rows.map do |record_data|
