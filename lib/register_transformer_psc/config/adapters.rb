openownership/register-transformer-psc

commit 642b882c88b860cb571ce9f735ad1fd9b2bb571e

[#188] refactor lib/

lib/register_transformer_psc/config/adapters.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require 'register_common/adapters/s3_adapter'
-require 'register_transformer_psc/config/settings'
+
+require 'settings'
 
 module RegisterTransformerPsc
   module Config

lib/register_transformer_psc/apps/transformer.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-require 'register_transformer_psc/config/settings'
-require 'register_sources_bods/services/publisher'
-require 'register_transformer_psc/bods_mapping/record_processor'
-require 'register_sources_psc/structs/company_record'
-require 'register_sources_oc/services/resolver_service'
 require 'register_common/services/stream_client_kinesis'
+require 'register_sources_bods/services/publisher'
+require 'register_sources_oc/services/resolver_service'
+require 'register_sources_psc/structs/company_record'
+
+require_relative '../bods_mapping/record_processor'
+require_relative '../config/settings'
 
 $stdout.sync = true
 

lib/register_transformer_psc/apps/transformer_bulk.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 require 'redis'
-
-require 'register_transformer_psc/config/settings'
-require 'register_transformer_psc/config/adapters'
-require 'register_sources_bods/services/publisher'
-require 'register_transformer_psc/bods_mapping/record_processor'
-require 'register_sources_psc/structs/company_record'
-require 'register_sources_oc/services/resolver_service'
 require 'register_common/services/file_reader'
+require 'register_sources_bods/services/publisher'
+require 'register_sources_oc/services/resolver_service'
+require 'register_sources_psc/structs/company_record'
+
+require_relative '../bods_mapping/record_processor'
+require_relative '../config/adapters'
+require_relative '../config/settings'
 
 $stdout.sync = true
 
 module RegisterTransformerPsc
   module Apps
     class TransformerBulk
-      BATCH_SIZE = 25
-      NAMESPACE = 'PSC_TRANSFORMER_BULK'
+      BATCH_SIZE     = 25
+      NAMESPACE      = 'PSC_TRANSFORMER_BULK'
       PARALLEL_FILES = ENV.fetch('PSC_PARALLEL_FILES', 3).to_i
 
       def self.bash_call(args)

lib/register_transformer_psc/apps/transformer_local.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-require 'register_transformer_psc/config/settings'
 require 'register_sources_bods/services/publisher'
-require 'register_transformer_psc/bods_mapping/record_processor'
-require 'register_sources_psc/structs/company_record'
 require 'register_sources_oc/services/resolver_service'
+require 'register_sources_psc/structs/company_record'
+
+require_relative '../bods_mapping/record_processor'
+require_relative '../config/settings'
 
 $stdout.sync = true
 

lib/register_transformer_psc/bods_mapping/child_entity_statement.rb
@@ -1,27 +1,23 @@
 # frozen_string_literal: true
 
+require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/object/try'
+require 'active_support/core_ext/string/conversions'
+require 'active_support/core_ext/time'
+require 'register_sources_bods/constants/publisher'
 require 'register_sources_bods/enums/entity_types'
 require 'register_sources_bods/enums/statement_types'
+require 'register_sources_bods/mappers/resolver_mappings'
 require 'register_sources_bods/structs/address'
 require 'register_sources_bods/structs/entity_statement'
 require 'register_sources_bods/structs/identifier'
 require 'register_sources_bods/structs/jurisdiction'
-require 'register_sources_bods/constants/publisher'
-require 'register_sources_bods/mappers/resolver_mappings'
-
-require 'active_support/core_ext/object/blank'
-require 'active_support/core_ext/object/try'
-require 'active_support/core_ext/time'
-require 'active_support/core_ext/string/conversions'
-
 require 'register_sources_oc/structs/resolver_request'
 
 module RegisterTransformerPsc
   module BodsMapping
     class ChildEntityStatement
       include RegisterSourcesBods::Mappers::ResolverMappings
-
-      ID_PREFIX = 'openownership-register-'
 
       def self.call(company_number, **kwargs)
         new(company_number, **kwargs).call

lib/register_transformer_psc/bods_mapping/entity_statement.rb
@@ -1,31 +1,26 @@
 # frozen_string_literal: true
 
-require 'uri'
-require 'xxhash'
-
+require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/object/try'
+require 'active_support/core_ext/string/conversions'
+require 'active_support/core_ext/time'
+require 'register_sources_bods/constants/publisher'
 require 'register_sources_bods/enums/entity_types'
 require 'register_sources_bods/enums/statement_types'
+require 'register_sources_bods/mappers/resolver_mappings'
 require 'register_sources_bods/structs/address'
 require 'register_sources_bods/structs/entity_statement'
 require 'register_sources_bods/structs/jurisdiction'
-require 'register_sources_bods/constants/publisher'
 require 'register_sources_bods/structs/publication_details'
 require 'register_sources_bods/structs/source'
-require 'register_sources_bods/mappers/resolver_mappings'
-
-require 'active_support/core_ext/object/blank'
-require 'active_support/core_ext/object/try'
-require 'active_support/core_ext/time'
-require 'active_support/core_ext/string/conversions'
-
 require 'register_sources_oc/structs/resolver_request'
+require 'uri'
+require 'xxhash'
 
 module RegisterTransformerPsc
   module BodsMapping
     class EntityStatement
       include RegisterSourcesBods::Mappers::ResolverMappings
-
-      ID_PREFIX = 'openownership-register-'
 
       def self.call(psc_record, **kwargs)
         new(psc_record, **kwargs).call

lib/register_transformer_psc/bods_mapping/interest_parser.rb
@@ -5,11 +5,11 @@ require 'register_sources_bods/structs/interest'
 module RegisterTransformerPsc
   module BodsMapping
     class InterestParser
+      UnexpectedInterestTypeError = Class.new(StandardError)
+
       def initialize(error_adapter: nil)
         @error_adapter = error_adapter
       end
-
-      UnexpectedInterestTypeError = Class.new(StandardError)
 
       def call(interest)
         case interest

lib/register_transformer_psc/bods_mapping/ownership_or_control_statement.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
-require 'xxhash'
-
+require 'register_sources_bods/constants/publisher'
+require 'register_sources_bods/structs/entity_statement'
 require 'register_sources_bods/structs/interest'
 require 'register_sources_bods/structs/ownership_or_control_statement'
-require 'register_sources_bods/structs/entity_statement'
-require 'register_sources_bods/structs/share'
-require 'register_sources_bods/constants/publisher'
 require 'register_sources_bods/structs/publication_details'
+require 'register_sources_bods/structs/share'
 require 'register_sources_bods/structs/source'
 require 'register_sources_bods/structs/subject'
+require 'xxhash'
 
 require_relative 'interest_parser'
 
@@ -17,8 +16,6 @@ module RegisterTransformerPsc
   module BodsMapping
     class OwnershipOrControlStatement
       UnsupportedSourceStatementTypeError = Class.new(StandardError)
-
-      ID_PREFIX = 'openownership-register-'
 
       def self.call(psc_record, **kwargs)
         new(psc_record, **kwargs).call

commit 7678ffbff9d09a47eb557b33a5c736a57447a922

[#188] fix Config::Adapters incorrect require
This was broken in 642b882c88b860cb571ce9f735ad1fd9b2bb571e during the
refactor.

lib/register_transformer_psc/config/adapters.rb
@@ -2,7 +2,7 @@
 
 require 'register_common/adapters/s3_adapter'
 
-require 'settings'
+require_relative 'settings'
 
 module RegisterTransformerPsc
   module Config